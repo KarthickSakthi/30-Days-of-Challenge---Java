e28bbfd1a7d403268fe8e477b592b0779661f7f9
Karthick S <56196782+KarthickSakthi@users.noreply.github.com>
2020-09-04 19:20:21 +0530

    Add files via upload

---
+++ StringUpperLowerComparison_day4.java
@@ -0,0 +1,28 @@
+
+
+
+import java.util.Scanner;
+public class StringUpperLowerComparison_day4
+{
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		String sk=s.next();
+		int upper=0,lower=0;
+		 for(int i=0;i<sk.length();i++){
+		     char c=sk.charAt(i);
+		     if(c>='A' && c<='Z'){
+		         upper++;
+		     }
+		     else if(c>='a' && c<='z'){
+		         lower++;
+		     }
+		 }
+		 if(upper==lower){
+		     System.out.println(sk.toLowerCase());
+		 }
+		 else{
+		    System.out.println(sk.toUpperCase());
+	}
+}
+}
+
