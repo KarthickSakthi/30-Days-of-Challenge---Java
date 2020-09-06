da39ef310b861fa69b8a7cce7c46a911ea9bd1d6
Karthick S <56196782+KarthickSakthi@users.noreply.github.com>
2020-09-06 10:26:59 +0530

    Add files via upload

---
+++ Primenumber_day6.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+ class Primenumber_day6
+{
+	public static void main(String[] args) {
+		
+		boolean isprime=true;
+		for(int n=1;n<=100;n++){
+		    if(n!=1){
+		    prime(n,isprime);}
+		}
+		
+		
+	}
+	static void prime(int n,boolean isprime){
+		for(int i=2;i<n;i++){
+		    if(n%i==0){
+		        isprime=false;
+		        break;
+		    }
+		}
+		if(isprime){
+		    System.out.print(n + " ");
+		}
+	
+	}
+}
+
