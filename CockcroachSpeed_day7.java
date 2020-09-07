181e145571501bcda609731129352a4e3ff1ca4c
Karthick S <56196782+KarthickSakthi@users.noreply.github.com>
2020-09-07 07:00:23 +0530

    Add files via upload

---
+++ CockcroachSpeed_day7.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+public class CockcroachSpeed_day7
+{
+	public static void main(String[] args) {
+	    Scanner s= new Scanner(System.in);
+	    int n=s.nextInt();// speed km/hr of cockroach
+	    double p=n*27.777778;
+		System.out.println("speed in cm/hr: "+p);
+	}
+}
+
