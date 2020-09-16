68958ab3cfa24bc6d861e958684362db23594ed0
Karthick S <56196782+KarthickSakthi@users.noreply.github.com>
2020-09-16 20:25:30 +0530

    Add files via upload

---
+++ Day16_Executing_Comments.java
@@ -0,0 +1,8 @@
+ class Day16_Executing_Comments  
+{  
+    public static void main(String[] args)   
+    {  
+         
+        // \u000d System.out.println("Executing Comments");  
+    }  
+}      
