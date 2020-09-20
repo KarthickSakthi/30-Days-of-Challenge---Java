2b441f5cd2ccdf1365521cf17fd442f9cf5c5044
Karthick S <56196782+KarthickSakthi@users.noreply.github.com>
2020-09-20 07:18:15 +0530

    Add files via upload

---
+++ Day18_Substring_count.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+class Day18_Substring_count
+{
+ public static void main(String args[])
+ {
+  Scanner s = new Scanner(System.in);
+  String str = s.next();
+  System.out.println("All substring of "+ str +" are:");
+   for (int i = 0; i < str.length(); i++) {
+   for (int j = i+1; j <= str.length(); j++) {
+         System.out.println(str.substring(i,j));
+   }}
+  
+ }
+}
