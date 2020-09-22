af3861c4de6eff21d9fbedfe786d9d6035cada67
Karthick S <56196782+KarthickSakthi@users.noreply.github.com>
2020-09-22 08:46:14 +0530

    Add files via upload

---
+++ Day22_Character_NumberoFtimes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Day22_Character_NumberoFtimes
+{
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String str,tempstr="";
+    str=sc.next();
+  for(int i=str.length()-1;i>=0;i--){
+    if(Character.isAlphabetic(str.charAt(i))){
+      int n=Integer.parseInt(str.substring(i+1));
+    for(int k=0;k<n;k++)
+     tempstr+=str.charAt(i);
+     str=str.substring(0,i);
+     }
+}
+System.out.println(new StringBuffer(tempstr).reverse().toString());
+}
+}
